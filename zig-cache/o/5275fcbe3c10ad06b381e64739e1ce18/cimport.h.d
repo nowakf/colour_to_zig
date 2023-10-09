07442b717bc15533e391029dd6700ad218ed5918
nowakf <francisnicholson@gmail.com>
2023-10-09 20:18:07 +0200

    ugh

--- zig-cache/o/5275fcbe3c10ad06b381e64739e1ce18/cimport.h.d
+++
@@ -1,4 +0,0 @@
-cimport.o: \
-  /home/francis/src/zig/sod/zig-cache/o/5275fcbe3c10ad06b381e64739e1ce18/cimport.h \
-  /home/francis/src/zig/sod/sod/sod.h /usr/lib/zig/include/stddef.h \
-  /usr/lib/zig/include/__stddef_max_align_t.h
