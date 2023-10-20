e9672f8fda8d34d1ff285cfbd817b5bd6381dc61
nowakf <francisnicholson@gmail.com>
2023-10-20 12:23:44 +0200

    chain_approx for contour tracing

--- src/moore.zig
+++ src/moore.zig
@@ -25,21 +25,12 @@ pub const Orientation = enum(u2) {
         }
 };
 
-test "realign" {
-    const expect = std.testing.expect;
-    const alc = std.testing.allocator;
-    const buf = try alc.alloc(u8, 64);
-    @memset(buf, 0);
-    try expect(!alc.resize(buf, 65));
-    const buf1 = try alc.realloc(buf, 65);
-    alc.free(buf1);
-}
-
 pub const Bitmap = struct {
     w: u32, 
     h: u32,
     bits: std.bit_set.DynamicBitSet,
     const Self = @This();
+
     fn at(self: Self, x: u32, y: u32) bool {
         return x < self.w and y < self.h and self.bits.isSet(y*self.w+x);
     }
@@ -51,7 +42,6 @@ pub const Bitmap = struct {
     fn deinit(self: *Self) void {
         self.bits.deinit();
     }
-
     fn from_img(alc: std.mem.Allocator, w: u32, h: u32, bytes: []u8) !Bitmap {
             _ = bytes;
             _ = h;
@@ -99,7 +89,7 @@ pub const Tracer = struct {
     orientation: Orientation = Orientation.N,
     pos: [2]u32,
     const Self = @This();
-    fn step(self: *Self, map: *const Bitmap) [2]u32 {
+    fn step(self: *Self, map: *const Bitmap) ?[2]i2 {
         const three_facing = self.orientation.three_facing();
         var bits = std.bit_set.IntegerBitSet(3).initEmpty();
         for (three_facing, 0..) |delta, i| {
@@ -108,65 +98,55 @@ pub const Tracer = struct {
             bits.setValue(2-i, map.at(x, y));
         }
         const three : u3 = bits.mask;
-        switch (three) {
-            0b100...0b111 => {
+        const new_heading = switch (three) {
+            0b100...0b111 => left: {
                 self.orientation.left();
-                self.pos = .{
-                    u32_i2_wrapping_sum(self.pos[0], three_facing[0][0]),
-                    u32_i2_wrapping_sum(self.pos[1], three_facing[0][1]),
-                };
+                break :left three_facing[0];
             },
-            0b010...0b011 => {
-                self.pos = .{
-                    u32_i2_wrapping_sum(self.pos[0], three_facing[1][0]),
-                    u32_i2_wrapping_sum(self.pos[1], three_facing[1][1]),
-                };
-            },
-            0b001         => {
-                self.pos = .{
-                    u32_i2_wrapping_sum(self.pos[0], three_facing[2][0]),
-                    u32_i2_wrapping_sum(self.pos[1], three_facing[2][1]),
-                };
-            },
+            0b010...0b011 => three_facing[1],
+            0b001         => three_facing[2],
             0b000         => {
                 self.orientation.right();
+                return null;
             },
-        }
-        return self.pos;
+        };
+
+        self.pos = .{
+                u32_i2_wrapping_sum(self.pos[0], new_heading[0]),
+                u32_i2_wrapping_sum(self.pos[1], new_heading[1]),
+        };
+
+        return new_heading;
     }
     pub fn find_next_start(map: *const Bitmap, start: [2]u32) ?[2]u32 {
         var it = map.bits.iterator(.{});
         it.bit_offset = start[1] / map.w + start[0];
         const next : u32 = @intCast(it.next().?);
-        return .{next / map.w, next % map.w};
+        return .{next % map.w, next / map.w};
     }
-
     pub fn trace(alc: std.mem.Allocator, map: *const Bitmap, start: [2]u32) !std.ArrayList([2]u32) {
         var contour = std.ArrayList([2]u32).init(alc);
-        try contour.append(start);
         var tracer = Tracer{
             .pos = start,
         };
 
         var right_turns : u32 = 0;
-        var last_loc = start;
-        var last_pushed = start;
+        var heading : [2]i2 = .{0, 0};
 
         while (true) {
-            const loc = tracer.step(map);
-            if (std.mem.eql(u32, &loc, &last_loc)) {
+            const prev = tracer.pos;
+            const next = tracer.step(map);
+            if (next == null) {
                 right_turns += 1;
             } else {
                 right_turns = 0;
-                last_loc = loc;
-                if (true) { //work out some chain-approx style simplification here
-                    last_pushed = loc;
-                    try contour.append(loc);
+                const n_heading = next.?;
+                if (n_heading[0] != heading[0] or n_heading[1] != heading[1]) {
+                    heading = n_heading;
+                    try contour.append(prev);
                 }
             }
-            const pts = contour.items;
-            if (right_turns == 3 or (right_turns == 0 and pts.len > 2 and std.mem.eql(u32, &pts[0], &pts[pts.len-1]))) {
-                _ = contour.pop();
+            if (right_turns == 3 or (right_turns == 0 and std.mem.eql(u32, &tracer.pos, &start))) {
                 return contour;
             }
         }
@@ -174,7 +154,6 @@ pub const Tracer = struct {
 };
 
 test "tracer_trace" {
-    const expect = std.testing.expect;
     const ContourSpec = struct {
         const Self = @This();
         map: Bitmap,
@@ -201,48 +180,79 @@ test "tracer_trace" {
                 .coords=managed,
             };
         }
+        fn eql(comptime N: comptime_int, comptime T: type, a: []const [N]T, b: []const [N]T) bool {
+            if (a.len != b.len) return false;
+            if (a.ptr == b.ptr) return true;
+            for (a, b) |a_elem, b_elem| {
+                if (!std.mem.eql(T, &a_elem, &b_elem)) return false;
+            }
+            return true;
+
+        }
+        fn check(self: Self, alc: std.mem.Allocator) !void {
+            const start = Tracer.find_next_start(&self.map, .{0, 0}) orelse
+                return error.NO_START;
+            if (!std.mem.eql(u32, &start, &self.coords.items[0])) {
+                std.debug.print("st: {any} actual: {any}\n", .{start, self.coords.items[0]});
+                return error.WRONG_START;
+            }
+            const traced = try Tracer.trace(alc, &self.map, start);
+            defer traced.deinit();
+            if (!Self.eql(2, u32, traced.items, self.coords.items)) {
+                std.debug.print("traced: {any} actual: {any}\n", .{traced.items, self.coords.items});
+                return error.NO_MATCH;
+            }
+        }
         fn deinit(self: *Self) void {
             self.coords.deinit();
             self.map.deinit();
         }
     };
+
+    const expect = std.testing.expect;
+    _ = expect;
+
     const alc = std.testing.allocator;
-    var empty = try ContourSpec.from_str(alc,
-        \\.  .  .  .  .  .  .  . 
-        \\.  .  .  .  .  .  .  . 
-        \\.  .  .  .  .  .  .  . 
-        \\.  .  .  .  .  .  .  . 
-        \\.  .  .  .  .  .  .  . 
-        \\.  .  .  .  .  .  .  . 
-        \\.  .  .  .  .  .  .  . 
-        \\.  .  .  .  .  .  .  .
-    );
-    defer empty.deinit();
-    expect(empty.coords.items.len == 0) catch |err| {
-        std.debug.print("{} {any}", .{err, empty.coords.items});
-        return err;
-    };
+
     var square = try ContourSpec.from_str(alc,
         //0  1  2  3  4  5  6  7
         \\.  .  .  .  .  .  .  . 
         \\.  .  .  .  .  .  .  . 
-        \\.  .  0  1  2  3  .  . 
-        \\.  .  b  .  .  4  .  . 
-        \\.  .  a  .  .  5  .  . 
-        \\.  .  9  8  7  6  .  . 
+        \\.  .  0  o  o  1  .  . 
+        \\.  .  o  .  .  o  .  . 
+        \\.  .  o  .  .  o  .  . 
+        \\.  .  3  o  o  2  .  . 
         \\.  .  .  .  .  .  .  . 
         \\.  .  .  .  .  .  .  .
     );
     defer square.deinit();
-    const start = Tracer.find_next_start(&square.map, .{0, 0}).?;
-    try expect(std.mem.eql(u32, &start, &.{2, 2}));
-    const traced = try Tracer.trace(alc, &square.map, start);
-    defer traced.deinit();
-    expect(square.coords.items.len == traced.items.len) catch |err|  {
-        std.debug.print("{}: \n{any}\n ------ \n {any}\n", .{err, square.coords.items, traced.items});
-        std.debug.print("{b:64}\n", .{square.map.bits.unmanaged.masks[0]});
-        return err;
-    };
+    try square.check(alc);
+    var bounds = try ContourSpec.from_str(alc,
+        //0  1  2  3  4  5  6  7
+        \\0  o  o  o  o  o  o  1 
+        \\o  .  .  .  .  .  .  o 
+        \\o  .  .  .  .  .  .  o 
+        \\o  .  .  .  .  .  .  o 
+        \\o  .  .  .  .  .  .  o 
+        \\o  .  .  .  .  .  .  o 
+        \\o  .  .  .  .  .  .  o 
+        \\3  o  o  o  o  o  o  2
+    );
+    defer bounds.deinit();
+    try bounds.check(alc);
+    var spiky = try ContourSpec.from_str(alc,
+        //0  1  2  3  4  5  6  7
+        \\.  .  .  .  .  .  .  . 
+        \\.  0  1  .  3  4  .  . 
+        \\.  .  8  2  o  .  .  . 
+        \\.  .  o  5  .  .  .  . 
+        \\.  .  o  o  .  .  .  . 
+        \\.  .  7  6  .  .  .  . 
+        \\.  .  .  .  .  .  .  . 
+        \\.  .  .  .  .  .  .  .
+    );
+    defer spiky.deinit();
+    try spiky.check(alc);
 }
 
 
