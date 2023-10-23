7cac7bb0c903ec3ff1100ace941725d26acc5b44
Florian Liesegang <lysigk@gmail.com>
2023-10-23 21:45:00 +0200

    clean up build.zig

--- build.zig
+++ build.zig
@@ -1,94 +1,94 @@
 const std = @import("std");
 const LazyPath = std.build.LazyPath;
 
-const c_source_files = [_][]const u8{
-    "vendor/sod/sod.c",
-};
-const c_flags = [_][]const u8{"-std=c99"};
 const raylib = @import("vendor/raylib/build.zig");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "sod",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
+        .name = "zig-video",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
-    raylib.addTo(b, exe, target, optimize);
+    // OPENPNP CAPTURE
+    const opnpc = b.addStaticLibrary(.{
+        .name = "zig-openpnp-capture",
+        .target = target,
+        .optimize = optimize,
+    });
 
-    exe.linkLibC();
-    exe.addIncludePath(LazyPath.relative("vendor/sod"));
-    exe.addCSourceFiles(.{.files = &c_source_files, .flags = &c_flags});
-    exe.defineCMacro("SOD_DISABLE_IMG_READER", "");
+    const t = opnpc.target_info.target;
 
-    if (target.getOsTag() == .macos) {
-        exe.addIncludePath(LazyPath.relative("vendor/openpnp-capture/"));
-        exe.addLibraryPath(LazyPath.relative("vendor/openpnp-capture/"));
-        exe.linkSystemLibrary("openpnp-capture");
+    opnpc.linkLibC();
+    opnpc.linkLibCpp();
+    opnpc.addIncludePath(.{ .path = "vendor/openpnp-capture/include" });
+    opnpc.addCSourceFiles(.{ .files = &opnpc_common_cpp_src_files });
+
+    switch (t.os.tag) {
+        .linux => {},
+        .macos => {
+            // openpnp_capture.defineCMacro("__PLATFORM__", "OSX 64");
+            opnpc.addCSourceFiles(.{
+                .files = &opnpc_objective_c_src_files,
+                // .flags = &.{"-fobjc-arc"},
+                .flags = &.{"-fno-objc-arc"},
+            });
+            opnpc.linkFramework("AVFoundation");
+            opnpc.linkFramework("Foundation");
+            opnpc.linkFramework("CoreMedia");
+            opnpc.linkFramework("CoreVideo");
+            opnpc.linkFramework("Accelerate");
+            opnpc.linkFramework("IOKit");
+        },
+        else => {},
     }
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
+    b.installArtifact(opnpc);
+
+    exe.linkLibrary(opnpc);
+    exe.addIncludePath(.{ .path = "vendor/openpnp-capture/include" });
+
+    // SOD
+    exe.linkLibC();
+    exe.addIncludePath(LazyPath.relative("vendor/sod"));
+    exe.addCSourceFiles(.{ .files = &sod_c_src_files, .flags = &sod_c_flags });
+    exe.defineCMacro("SOD_DISABLE_IMG_READER", "");
+
+    // RAYLIB
+    raylib.addTo(b, exe, target, optimize);
+
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    unit_tests.linkLibC();
-    unit_tests.addIncludePath(LazyPath.relative("vendor/sod"));
-    unit_tests.addCSourceFiles(.{.files = &c_source_files, .flags = &c_flags});
-
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
 }
+
+const opnpc_common_cpp_src_files = [_][]const u8{
+    "vendor/openpnp-capture/common/context.cpp",
+    "vendor/openpnp-capture/common/libmain.cpp",
+    "vendor/openpnp-capture/common/logging.cpp",
+    "vendor/openpnp-capture/common/stream.cpp",
+};
+
+const opnpc_objective_c_src_files = [_][]const u8{
+    "vendor/openpnp-capture/mac/platformcontext.mm",
+    "vendor/openpnp-capture/mac/platformstream.mm",
+    "vendor/openpnp-capture/mac/uvcctrl.mm",
+};
+
+const sod_c_flags = [_][]const u8{"-std=c99"};
+
+const sod_c_src_files = [_][]const u8{
+    "vendor/sod/sod.c",
+};
