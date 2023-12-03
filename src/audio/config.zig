5344834f183c2aad9a1985a5319c826026451465
Florian Liesegang <lysigk@gmail.com>
2023-12-03 14:55:00 +0100

    add comb section

--- src/audio/config.zig
+++ src/audio/config.zig
@@ -9,8 +9,8 @@ pub const MIN_FREQ = 100;
 pub const MAX_FREQ = 10_000;
 
 pub const ATTACK = 0.001;
-pub const MIN_DECAY = 0.5;
-pub const MAX_DECAY = 4;
+pub const MIN_DECAY = 0.1;
+pub const MAX_DECAY = 2;
 
 // DELAY
 pub const MAX_DELAY_LENGTH = SR * 10;
