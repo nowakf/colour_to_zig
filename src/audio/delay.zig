ab5d116141b5cae8a5b7ac88cf3c59dd748781a9
Florian Liesegang <lysigk@gmail.com>
2023-12-04 21:29:59 +0100

    add resonance to lpf

--- src/audio/delay.zig
+++ src/audio/delay.zig
@@ -18,14 +18,17 @@ pub const Delay = struct {
 
     lpf: LPF,
 
-    prng: std.rand.DefaultPrng,
+    rand: std.rand.Random,
 
     pub fn init(min_dur: usize, max_dur: usize, fb: f32) Delay {
         // TODO: Use shared PRNG
         var prng = std.rand.DefaultPrng.init(0);
+        const rand = prng.random();
 
         const max_d = if (max_dur <= conf.MAX_DELAY_LENGTH) max_dur else conf.MAX_DELAY_LENGTH;
-        const dur = prng.random().intRangeAtMost(usize, min_dur, max_d);
+        const dur = rand.intRangeAtMost(usize, min_dur, max_d);
+
+        const lpf_freq = rand.float(f32) * 10000 + 2000;
 
         return .{
             .buf = [_]f32{0.0} ** conf.MAX_DELAY_LENGTH,
@@ -33,14 +36,15 @@ pub const Delay = struct {
             .max_dur = max_d,
             .del_dur = dur,
             .fb = fb,
-            .prng = prng,
-            .lpf = LPF.init(0.125),
+            .rand = rand,
+            .lpf = LPF.init(lpf_freq, 2.75),
             .var_dur = conf.SR * 4,
         };
     }
 
     pub fn sample(self: *Delay, in: f32) f32 {
-        const s = self.buf[self.del_idx];
+        const noise = self.rand.float(f32) * 2.0 - 1.0;
+        const s = self.buf[self.del_idx] + noise;
         self.advance();
         self.buf[self.del_idx] = self.lpf.sample((self.buf[self.del_idx] + in) * self.fb);
         return s;
@@ -56,10 +60,9 @@ pub const Delay = struct {
         if (self.var_idx < self.var_dur - 1) {
             self.var_idx += 1;
         } else {
-            const rand = self.prng.random();
-            self.del_dur = rand.intRangeAtMost(usize, self.min_dur, self.max_dur);
-            self.var_dur = rand.intRangeAtMost(usize, self.min_dur, self.max_dur);
-            self.lpf.cut = rand.float(f32);
+            self.del_dur = self.rand.intRangeAtMost(usize, self.min_dur, self.max_dur);
+            self.var_dur = self.rand.intRangeAtMost(usize, self.min_dur, self.max_dur);
+            self.lpf.freq = self.rand.float(f32) * 10000 + 2000;
             self.var_idx = 0;
         }
     }
--- src/audio/lpf.zig
+++ src/audio/lpf.zig
@@ -1,18 +1,38 @@
+const std = @import("std");
+const math = std.math;
+
 const conf = @import("config.zig");
 
 pub const LPF = struct {
-    cut: f32,
-    last: f32 = 0.0,
+    freq: f32,
+    res: f32,
+    s1: f32 = 0.0,
+    s2: f32 = 0.0,
 
-    pub fn init(cut: f32) LPF {
+    pub fn init(freq: f32, res: f32) LPF {
         return .{
-            .cut = cut,
+            .freq = freq,
+            .res = res,
         };
     }
 
     pub fn sample(self: *LPF, in: f32) f32 {
-        const diff = in - self.last;
-        self.last += diff * self.cut;
-        return self.last;
+        const g = @tan(math.pi * self.freq / conf.SR);
+        const damping = 1.0 / self.res;
+        const a1 = 1.0 / (1.0 + g * (g + damping));
+        const a2 = g * a1;
+        const a3 = g * a2;
+        const m0 = 0.0;
+        const m1 = 0.0;
+        const m2 = 1.0;
+
+        const y0 = (m0 * in + m1 * self.s1 + m2 * self.s2) * a1;
+        const y1 = a2 * (in - y0) + self.s1;
+        const y2 = a3 * (in - y0) + self.s2;
+
+        self.s1 = 2.0 * y1 - self.s1;
+        self.s2 = 2.0 * y2 - self.s2;
+
+        return y0;
     }
 };
